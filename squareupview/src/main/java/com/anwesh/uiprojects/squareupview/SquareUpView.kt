2d9b352ed35f0ef5176c3aa8df225b7d37662433
Anwesh43 <anweshthecool0@gmail.com>
2019-09-30 10:24:59 +0530

    adding SUPNode class

--- squareupview/src/main/java/com/anwesh/uiprojects/squareupview/SquareUpView.kt
+++ squareupview/src/main/java/com/anwesh/uiprojects/squareupview/SquareUpView.kt
@@ -119,4 +119,46 @@ class SquareUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SUPNode(var i : Int, val state : State = State()) {
+
+        private var next : SUPNode? = null
+        private var prev : SUPNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SUPNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSUPNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SUPNode {
+            var curr : SUPNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
