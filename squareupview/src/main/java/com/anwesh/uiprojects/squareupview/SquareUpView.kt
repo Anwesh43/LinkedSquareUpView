643a158a78f16b3c3b3ca2c3d1dad8c53df6c61b
Anwesh43 <anweshthecool0@gmail.com>
2019-09-30 09:38:57 +0530

    adding Int and Float extension methods

--- squareupview/src/main/java/com/anwesh/uiprojects/squareupview/SquareUpView.kt
+++ squareupview/src/main/java/com/anwesh/uiprojects/squareupview/SquareUpView.kt
@@ -21,3 +21,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val squareColor : Int = Color.parseColor("#2196F3")
 val circleColor : Int = Color.parseColor("#f44336")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
